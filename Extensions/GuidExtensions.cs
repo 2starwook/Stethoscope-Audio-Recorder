70f6356d198399298f5a9058ddf31ef04f976e22
Isaac Lee <2starwook@gmail.com>
2023-11-07 11:58:26 -0600

    Add comments for full uuid

--- Extensions/GuidExtensions.cs
+++ Extensions/GuidExtensions.cs
@@ -4,13 +4,12 @@ public static class GuidExtensions
 {
     public static Guid UuidFromPartial(this Int32 @partial)
     {
-        //this sometimes returns only the significant bits, e.g.
-        //180d or whatever. so we need to add the full string
         string id = @partial.ToString("X").PadRight(4, '0');
 
         if (id.Length == 4)
         {
             id = "0000" + id + "-0000-1000-8000-00805f9b34fb";
+            //Default BLE uuid full 128 bits value
         }
 
         return Guid.ParseExact(id, "d");
