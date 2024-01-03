7bb7a8c2c9742915bd55c0d4202eeab01cc04041
Isaac Lee <2starwook@gmail.com>
2024-01-03 16:01:27 -0500

    Add more data on graph

--- Pages/GraphPage.xaml.cs
+++ Pages/GraphPage.xaml.cs
@@ -7,32 +7,37 @@ namespace NET_MAUI_BLE.Pages;
 
 
 public partial class GraphPage : ContentPage {
+	ChartEntry[] data = new[]
+	{
+			// Label = "112",
+			// ValueLabel = "112",
+		new ChartEntry(0){},
+		new ChartEntry(100){},
+		new ChartEntry(0){},
+		new ChartEntry(100){},
+		new ChartEntry(0){},
+		new ChartEntry(100){},
+		new ChartEntry(0){},
+		new ChartEntry(100){},
+		new ChartEntry(0){},
+		new ChartEntry(100){},
+		new ChartEntry(0){},
+		new ChartEntry(100){},
+		new ChartEntry(0){},
+		new ChartEntry(100){},
+		new ChartEntry(0){},
+	};
 
 	public GraphPage() {
 		InitializeComponent();
 		try {
 			chartView.Chart = new LineChart {
-				Entries = entries
+				Entries = data
 			};
 		}
 		catch (Exception e){
 			System.Diagnostics.Debug.WriteLine(e.ToString());
 		}
 	}
-	ChartEntry[] entries = new[]
-	{
-		new ChartEntry(212){
-			Label = "Windows",
-			ValueLabel = "112",
-		},
-		new ChartEntry(248){
-			Label = "Android",
-			ValueLabel = "648",
-		},
-		new ChartEntry(128){
-			Label = "iOS",
-			ValueLabel = "428",
-		}
-	};
 }
 
