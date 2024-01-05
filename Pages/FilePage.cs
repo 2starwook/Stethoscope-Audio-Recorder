d7b5323cc4554c3524cb1a125ed20901757e68a9
Isaac Lee <2starwook@gmail.com>
2024-01-05 16:32:18 -0500

    Add TODO lists

--- Pages/FilePage.cs
+++ Pages/FilePage.cs
@@ -11,9 +11,12 @@ public partial class FilePage : ContentPage {
 	private DatabaseManager databaseManager;
 	private AudioController audioController;
 
-	// TODO - Connect AudioController/Player to DatabaseManager
-	// TODO - Add EventHandler to each button to start music
-	// TODO - Implement: Rename each data (not real data name)
+	// TODO - Implement: Page for each record (Play/Pause/Stop Sound / Rename Title / Share / Exam Details )
+	// TODO - Implement: Rename each record (not real data name instead of path)
+	// TODO - Idea: (From left to right - Setting / Home / Records)
+	// Setting - Background connection time (Default 5 min) / Recording length (default 15sec)
+	// Home - Record the sound (Save / Create new patient option)
+	// Records - Show list of records by each patients (Search bar, if there is any record / No Record, Otherwise)
 	public FilePage(IAudioManager audioManager) {
 		this.databaseManager = new DatabaseManager();
 		this.audioController = new AudioController(audioManager);
