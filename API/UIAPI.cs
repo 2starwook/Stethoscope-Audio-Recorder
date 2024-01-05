35615ec17c2d895ccae4c8f6e086d57f7fdb35cb
Isaac Lee <2starwook@gmail.com>
2024-01-05 16:32:51 -0500

    Add styleId para to UIAPI CreateButton

--- API/UIAPI.cs
+++ API/UIAPI.cs
@@ -1,9 +1,13 @@
 namespace MYAPI;
 public static class UIAPI{
-	public static Button CreateButton(string text, EventHandler e) {
+	public static Button CreateButton(
+		string text, EventHandler e=null, string styleId=null) {
 		Button button = new Button { Text = text };
 		if (e != null){
 			button.Clicked += e;
+		}
+		if (styleId != null){
+			button.StyleId = styleId;
 		}
 		return button;
 	}
