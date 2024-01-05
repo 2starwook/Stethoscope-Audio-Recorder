43725fdf086d07133d7d7a1990652b74dce961cb
Isaac Lee <2starwook@gmail.com>
2024-01-05 12:01:01 -0500

    Add Patient class

---
+++ Object/Patient.cs
@@ -0,0 +1,16 @@
+
+
+namespace Object.MyPatient;
+public class Patient
+{
+    private string name;
+    // TODO - Add more information
+
+	public Patient(string name) {
+        this.name = name;
+	}
+
+    public string GetName() {
+        return this.name;
+    }
+}
