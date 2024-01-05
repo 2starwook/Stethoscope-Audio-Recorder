7099e660c9a419c5b984a985751cad44ab3a1cff
Isaac Lee <2starwook@gmail.com>
2024-01-05 13:18:45 -0500

    Add id and modify Name to Patient class

--- Object/Patient.cs
+++ Object/Patient.cs
@@ -3,14 +3,13 @@
 namespace Object.MyPatient;
 public class Patient
 {
-    private string name;
+    public readonly string id;
+    public string Name {get; set;}
     // TODO - Add more information
 
-	public Patient(string name) {
-        this.name = name;
+	public Patient(string id, string name) {
+        this.id = id;
+        Name = name;
 	}
 
-    public string GetName() {
-        return this.name;
-    }
 }
