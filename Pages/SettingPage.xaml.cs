27a6cd6ab3ab7e23e65bbfb2156c9fe09255deb6
Isaac Lee <2starwook@gmail.com>
2024-01-05 09:53:10 -0500

    Add comments for SettingPage

--- Pages/SettingPage.xaml.cs
+++ Pages/SettingPage.xaml.cs
@@ -1,6 +1,7 @@
-﻿namespace NET_MAUI_BLE.Pages;
+namespace NET_MAUI_BLE.Pages;
 
 public partial class SettingPage : ContentPage {
+	// Controller settings for stethoscope device
 	public SettingPage()
 	{
 		InitializeComponent();
