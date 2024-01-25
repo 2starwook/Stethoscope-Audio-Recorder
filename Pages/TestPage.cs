e65b4cba51dff144227dd7d592bafa6c5ed699db
Isaac Lee <2starwook@gmail.com>
2024-01-25 16:25:14 -0600

    Add TestViewModel

--- Pages/TestPage.cs
+++ Pages/TestPage.cs
@@ -1,54 +1,13 @@
-﻿using System.IO;
-using System.Diagnostics;
-using System.Globalization;
-using CommunityToolkit.Maui.Storage;
-using Microsoft.Maui.Media;
-
-using static Config;
-using Object.MyFileController;
-using Object.MyAudio;
-using Plugin.Maui.Audio;
-
+﻿using NET_MAUI_BLE.ViewModel;
 
 namespace NET_MAUI_BLE.Pages;
 
 public partial class TestPage : ContentPage {
-	private AudioController audioController;
-	private FileController fileController;
-
-	public TestPage(IAudioManager audioManager) {
+	
+	public TestPage(TestViewModel vm)
+	{
 		InitializeComponent();
-		this.audioController = new AudioController(audioManager);
-		this.fileController = new FileController();
+		BindingContext = vm;
 	}
-
-	private async void Btn1Clicked(object sender, EventArgs e) {
-		try {
-			var path = await this.fileController.GetDeviceFilePath();
-			if (path != null)
-			{
-				label.Text = path;
-			}
-		}
-		catch {
-			// Exception thrown when user cancels
-		}
-    }
-	private async void Btn2Clicked(object sender, EventArgs e) {
-		try {
-			audioController.OpenFile(label.Text);
-			if (audioController.IsPlaying()) {
-				audioController.Stop();
-			}
-			else {
-				audioController.Play();
-			}
-
-		}
-		catch {
-			// Exception thrown when user cancels
-		}
-    }
-
 }
 
---
+++ ViewModel/TestViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Plugin.Maui.Audio;
+
+using Object.MyData;
+
+namespace NET_MAUI_BLE.ViewModel;
+
+public partial class TestViewModel : ObservableObject
+{
+	public TestViewModel(DatabaseManager databaseManager)
+	{
+        this.databaseManager = databaseManager;
+		patients = new ObservableCollection<string>();
+        // TODO - Once patients data is ready, add here
+        //foreach (var each in databaseManager.GetPathList())
+        //{
+        //    Patients.Add(each);
+        //}
+    }
+
+    private DatabaseManager databaseManager;
+
+    [ObservableProperty]
+    ObservableCollection<string> patients;
+
+    [ObservableProperty]
+	string name;
+
+	[ObservableProperty]
+	string nameLabel;
+
+	[RelayCommand]
+	void Submit()
+	{
+		NameLabel = Name;
+	}
+}
