8604c0de030e69094d8ee70e526bb5c64200e1df
Isaac Lee <2starwook@gmail.com>
2024-01-25 16:55:00 -0600

    Implement SelectFileCommand

--- ViewModel/AddViewModel.cs
+++ ViewModel/AddViewModel.cs
@@ -13,6 +13,7 @@ public partial class AddViewModel : ObservableObject
 	{
         this.databaseManager = databaseManager;
 		patients = new ObservableCollection<string>();
+        fileButtonText = "Select a File";
         // TODO - Once patients data is ready, add here
         //foreach (var each in databaseManager.GetPathList())
         //{
@@ -31,9 +32,22 @@ public partial class AddViewModel : ObservableObject
 	[ObservableProperty]
 	string nameLabel;
 
+    [ObservableProperty]
+    string fileButtonText;
+
 	[RelayCommand]
 	void Submit()
 	{
 		NameLabel = Name;
 	}
+
+    [RelayCommand]
+    async Task SelectFile()
+    {
+        var path = await databaseManager.ImportAudioFile();
+        if (string.IsNullOrWhiteSpace(path))
+            return;
+        var filename = Path.GetFileName(path);
+        FileButtonText = filename;
+    }
 }
