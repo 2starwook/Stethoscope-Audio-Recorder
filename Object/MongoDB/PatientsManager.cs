316086fc0357488c6e75c00dd91c602733c73f42
Isaac Lee <2starwook@gmail.com>
2024-02-13 19:54:36 -0600

    Implement methods

--- Object/MongoDB/PatientsManager.cs
+++ Object/MongoDB/PatientsManager.cs
@@ -15,6 +15,11 @@ public class PatientsManager<T> where T : Patient
         this._mongoDB = new MongoDB<T>(collectionName);
 	}
 
+    public List<T> GetPatients()
+    {
+        return _mongoDB.GetItems();
+    }
+
     public async void UpdatePatientFirstName(string id, string firstName)
     {
         var filter = Builders<T>.Filter.Eq(p => p.GetId(), id);
@@ -22,7 +27,23 @@ public class PatientsManager<T> where T : Patient
             .Set(p => p.FirstName, firstName);
         await _mongoDB.collection.UpdateOneAsync(filter, update);
     }
-    // TODO - Add more Update Method
+    public async void UpdatePatientLastName(string id, string lastName)
+    {
+        var filter = Builders<T>.Filter.Eq(p => p.GetId(), id);
+        var update = Builders<T>.Update
+            .Set(p => p.LastName, lastName);
+        await _mongoDB.collection.UpdateOneAsync(filter, update);
+    }
+
+    public async Task InsertPatientAsync(T item)
+    {
+        await _mongoDB.InsertItemAsync(item);
+    }
+
+    public async Task DeletePatientAsync(string id)
+    {
+        await _mongoDB.DeleteItemAsync(id);
+    }
 
 
 }
