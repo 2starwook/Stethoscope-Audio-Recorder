58e73ac579dc8f7876ce0ab91f1ebec723005660
Isaac Lee <2starwook@gmail.com>
2024-02-13 21:26:40 -0600

    Implement Patient related method

--- Object/DatabaseManager.cs
+++ Object/DatabaseManager.cs
@@ -11,14 +11,20 @@ public class DatabaseManager
     private PatientsManager<Patient> patientsManager;
     private RecordsManager<Record> recordsManager;
     public Dictionary<string, Record> currentRecords;
+    public Dictionary<string, Patient> currentPatients;
     
 	public DatabaseManager() {
         this.patientsManager = new PatientsManager<Patient>();
         this.recordsManager = new RecordsManager<Record>();
         currentRecords = new Dictionary<string, Record>();
-        foreach (Record r in recordsManager.GetItems())
+        foreach (Record r in recordsManager.GetRecords())
         {
             currentRecords.Add(r.GetId(), r);
+        }
+        currentPatients = new Dictionary<string, Patient>();
+        foreach (Patient p in patientsManager.GetPatients())
+        {
+            currentPatients.Add(p.GetId(), p);
         }
 	}
 
@@ -32,28 +38,17 @@ public class DatabaseManager
         await recordsManager.DeleteRecordAsync(id);
         currentRecords.Remove(id);
     }
-        var record = new Record(recordName, File.ReadAllBytes(audioFilePath), null);
-        await recordsManager.InsertItemAsync(record);
-        currentRecords.Add(FileAPI.GetUniqueID(), record);
+
+    public async Task AddPatientAsync(string firstName, string lastName){
+        var patient = new Patient(firstName, lastName);
+        await patientsManager.InsertPatientAsync(patient);
+        currentPatients.Add(FileAPI.GetUniqueID(), patient);
     }
 
-    public async Task DeleteRecordDataAsync(string id){
-        await recordsManager.DeleteItemAsync(id);
-        currentRecords.Remove(id);
+    public async Task DeletePatientAsync(string id){
+        await patientsManager.DeletePatientAsync(id);
+        currentPatients.Remove(id);
     }
-
-    #pragma warning disable CS1998
-    public async Task AddPatientDataAsync(){
-        // TODO - Implement Later
-    }
-    #pragma warning restore CS1998
-
-    #pragma warning disable CS1998
-    public async Task DeletePatientDataAsync(string id){
-        // TODO - Implement Later
-    }
-    #pragma warning restore CS1998
-
 
     public void AttachPatientToRecord(string audioFilePath, string patientId){
         // TODO - Implement Later
@@ -62,10 +57,6 @@ public class DatabaseManager
     public void DetachAssignedPatientFromRecord(string audioFilePath){
         // TODO - Implement Later
     }
-
-    // public bool IsDataExist(string audioFilePath){
-    //     return recordCollection.IsExist(audioFilePath);
-    // }
 
     public async Task<string> ImportAudioFile(){
         var srcPath = await StorageAPI.GetFilePath();
