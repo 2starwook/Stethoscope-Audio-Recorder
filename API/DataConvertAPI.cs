039c4f1ee958328d8db785f3bfb367c0ed37db59
Isaac Lee <2starwook@gmail.com>
2024-02-27 18:39:06 -0600

    Add Combine method

--- API/DataConvertAPI.cs
+++ API/DataConvertAPI.cs
@@ -1,5 +1,6 @@
 using System.Text;
 
+
 namespace MYAPI;
 static public class DataConvertAPI
 {
@@ -7,5 +8,11 @@ static public class DataConvertAPI
         return Encoding.Default.GetBytes(data);
     }
 
-
+    static public byte[] Combine(byte[] first, byte[] second)
+    {
+        byte[] ret = new byte[first.Length + second.Length];
+        Buffer.BlockCopy(first, 0, ret, 0, first.Length);
+        Buffer.BlockCopy(second, 0, ret, first.Length, second.Length);
+        return ret;
+    }
 }
