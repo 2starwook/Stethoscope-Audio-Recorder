303fe58ff0143ada229b7d7d4032ed014514f6fa
Isaac Lee <2starwook@gmail.com>
2024-03-05 19:31:40 -0600

    Separate PatientInfo

---
+++ Object/PatientInfo.cs
@@ -0,0 +1,11 @@
+namespace Object.Frontend;
+public class PatientInfo
+{
+    public string name { get; set; }
+    public string id { get; set; }
+    public PatientInfo(string name, string id)
+    {
+        this.name = name;
+        this.id = id;
+    }
+}
--- ViewModel/AddRecordViewModel.cs
+++ ViewModel/AddRecordViewModel.cs
@@ -3,22 +3,11 @@ using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 
 using Object.MyData;
+using Object.Frontend;
 using NET_MAUI_BLE.Pages;
 
 
 namespace NET_MAUI_BLE.ViewModel;
-
-public class PatientInfo
-{
-    public string name { get; set; }
-    public string id { get; set; }
-    public PatientInfo(string name, string id)
-    {
-        this.name = name;
-        this.id = id;
-    }
-}
-
 public partial class AddRecordViewModel : ObservableObject
 {
 	public AddRecordViewModel(DatabaseManager databaseManager)
