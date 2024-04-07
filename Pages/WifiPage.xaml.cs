d17a89ddea8c66fd87fcaf948e19fb43b8241103
Isaac Lee <2starwook@gmail.com>
2024-04-07 18:06:37 -0500

    Attach WifiViewModel to WifiPage

--- Pages/WifiPage.xaml.cs
+++ Pages/WifiPage.xaml.cs
@@ -1,9 +1,13 @@
+using NET_MAUI_BLE.ViewModel;
+
+
 namespace NET_MAUI_BLE.Pages;
 
 public partial class WifiPage : ContentPage
 {
-	public WifiPage()
+	public WifiPage(WifiViewModel vm)
 	{
 		InitializeComponent();
+		BindingContext = vm;
 	}
 }
