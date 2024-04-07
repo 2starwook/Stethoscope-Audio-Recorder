d66892f2eba8b603e9f071b576b835bec61995ea
Isaac Lee <2starwook@gmail.com>
2024-04-07 18:05:12 -0500

    Handle whitespace

--- Pages/TestPage.xaml.cs
+++ Pages/TestPage.xaml.cs
@@ -1,10 +1,10 @@
 using NET_MAUI_BLE.ViewModel;
 
+
 namespace NET_MAUI_BLE.Pages;
 
 public partial class TestPage : ContentPage
 {
-
     public TestPage(TestViewModel vm)
     {
         InitializeComponent();
