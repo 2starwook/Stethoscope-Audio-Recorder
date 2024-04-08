816a346619709729629202d708876a4d9d8abd40
Isaac Lee <2starwook@gmail.com>
2024-04-08 11:20:37 -0500

    Add AudioControl

---
+++ Control/AudioControl.xaml.cs
@@ -0,0 +1,100 @@
+using CommunityToolkit.Mvvm.Input;
+
+using NET_MAUI_BLE.Object.Audio;
+using NET_MAUI_BLE.API;
+using Plugin.Maui.Audio;
+
+
+namespace NET_MAUI_BLE.Controls;
+
+public partial class AudioControl : ContentView
+{
+    public AudioControl()
+    {
+        _audioController = new AudioController(AudioManager.Current);
+        PlayingView = false;
+        PauseResumeText = "Pause";
+        InitializeComponent();
+    }
+    private AudioController _audioController;
+    private bool _playingView;
+    public bool PlayingView {
+        get => _playingView;
+        set
+        {
+            _playingView = value;
+            OnPropertyChanged(nameof(PlayingView));
+        }
+    }
+    private string _pauseResumeText;
+    public string PauseResumeText {
+        get => _pauseResumeText;
+        set
+        {
+            _pauseResumeText = value;
+            OnPropertyChanged(nameof(PauseResumeText));
+        }
+    }
+
+    public string Source
+    {
+        get => GetValue(SourceProperty) as string;
+        set => SetValue(SourceProperty, value);
+    }
+
+    public static readonly BindableProperty SourceProperty = BindableProperty.Create(
+        nameof(Source), typeof(string), typeof(AudioControl),
+        propertyChanged: (bindable, oldValue, newValue) =>
+        {
+            var control = (AudioControl)bindable;
+        },
+        validateValue: (bindable, value) =>
+        {
+            return FileAPI.isExist(FileAPI.GetCachePath(value.ToString()));
+        }
+        );
+
+    [RelayCommand]
+    public async Task Share()
+    {
+        var tempName = "file.wav";
+        var binaryData = FileAPI.ReadCacheData(Source);
+        await StorageAPI.ExportData(tempName, binaryData);
+    }
+
+    [RelayCommand]
+    public async Task Play()
+    {
+        PlayingView = true;
+        await _audioController.OpenFile(FileAPI.GetCachePath(Source));
+        _audioController.Play();
+        _audioController.AddEventHandler(new EventHandler(HandlePlayEnded));
+    }
+
+    void HandlePlayEnded(object sender, EventArgs e)
+    {
+        PlayingView = false;
+    }
+
+    [RelayCommand]
+    public void PauseResume()
+    {
+        if (_audioController.IsPlaying())
+        {
+            _audioController.Pause();
+            PauseResumeText = "Resume";
+        }
+        else
+        {
+            _audioController.Play();
+            PauseResumeText = "Pause";
+        }
+    }
+
+    [RelayCommand]
+    public void Stop()
+    {
+        PlayingView = false;
+        _audioController.Stop();
+    }
+}
