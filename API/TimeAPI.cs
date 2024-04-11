6197504d134cbb8d75f1cf2c5e6a736c68f035bd
Isaac Lee <2starwook@gmail.com>
2024-04-11 15:37:51 -0500

    Add TimeAPI

---
+++ API/TimeAPI.cs
@@ -0,0 +1,12 @@
+﻿using System.Text;
+
+
+namespace NET_MAUI_BLE.API;
+
+static public class TimeAPI
+{
+    static public string GetCurrentDateTime()
+    {
+        return DateTime.Now.ToString("yyyyMMddHHmmssffff");
+    }
+}
