128793c49810f359ac544b51e39b0db78fa5af86
Isaac Lee <2starwook@gmail.com>
2024-04-11 12:59:11 -0500

    Add GetAudio method

--- Object/WifiController.cs
+++ Object/WifiController.cs
@@ -1,5 +1,5 @@
-using System.Text;
-using System.Text.Json;
+using NET_MAUI_BLE.API;
+using NET_MAUI_BLE.AppConfig;
 
 
 namespace NET_MAUI_BLE.Object.Wifi;
@@ -8,11 +8,20 @@ public class WifiController
 {
     public WifiController()
     {
-        _baseAddress = "";
+        _baseAddress = Config.HTTP_BASE_ADDRESS_TEST;
         _httpClientManager = new HttpClientManager(_baseAddress);
     }
 
     private HttpClientManager _httpClientManager;
     private string _baseAddress;
 
+    public async Task<string> GetAudio()
+    {
+        var unique_id = FileAPI.GetUniqueID();
+        var fileName = $"{unique_id}.wav";
+        var binaryData = await _httpClientManager.GetBinaryAsync("/R10");
+        FileAPI.WriteCacheData(fileName, binaryData);
+        // TODO - Reusing a single path for temporary files
+        return FileAPI.GetCachePath(fileName);
+    }
 }
