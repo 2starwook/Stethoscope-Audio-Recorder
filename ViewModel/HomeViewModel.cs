12e3fca2921a0308e86835b0c461abcaedff5a6a
Isaac Lee <2starwook@gmail.com>
2024-04-11 14:21:20 -0500

    Move contents from WifiPage to HomePage

--- ViewModel/HomeViewModel.cs
+++ ViewModel/HomeViewModel.cs
@@ -1,180 +1,75 @@
-using CommunityToolkit.Mvvm.Messaging;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using System.Text;
+using System.Text.Json;
 
-using NET_MAUI_BLE.Message.BleMessage;
-using NET_MAUI_BLE.Object.BLE;
-using NET_MAUI_BLE.Enum;
-using NET_MAUI_BLE.API;
+using NET_MAUI_BLE.Object.Wifi;
 
 
 namespace NET_MAUI_BLE.ViewModel;
 
-public partial class HomeViewModel : ObservableObject, IRecipient<BleDataMessage>, IRecipient<BleStatusMessage>
+public partial class HomeViewModel : ObservableObject
 {
 	public HomeViewModel()
 	{
-        _bleController = new BleController();
-        WeakReferenceMessenger.Default.Register<BleDataMessage>(this);
-        WeakReferenceMessenger.Default.Register<BleStatusMessage>(this);
-        ResultText = "Waiting to be connected...";
-        BleStatusText = "Scanning";
-
-        CurrentBleStatus = BleStatus.Scanning;
-
-        CurrentRecordStatus = RecordStatus.Off;
-        RecordView = false;
-        RecordButtonView = true;
-        PauseButtonView = false;
-        ResumeButtonView = false;
+        TestLabel = "Empty";
+        wifiController = new WifiController();
+        AudioControlVisible = false;
+        audioSource = "";
     }
-    public byte[] receivedFile = new byte[] {};
 
-    private BleController _bleController;
+    private WifiController wifiController;
     [ObservableProperty]
-    private string resultText;
+    string testLabel;
     [ObservableProperty]
-    private string bleStatusText;
+    bool audioControlVisible;
+    [ObservableProperty]
+    string audioSource;
 
-    [ObservableProperty]
-    BleStatus currentBleStatus;
-    [ObservableProperty]
-    RecordStatus currentRecordStatus;
-    [ObservableProperty]
-    private bool recordView;
-    [ObservableProperty]
-    private bool recordButtonView;
-    [ObservableProperty]
-    private bool pauseButtonView;
-    [ObservableProperty]
-    private bool resumeButtonView;
-
-    #pragma warning disable CS1998
     [RelayCommand]
-    async Task Appearing()
+    void Appearing()
     {
         try
         {
-            //NOTE - Remove comments if Ble needed
-            //await InitiateBle();
+
         }
         catch (Exception e)
         {
             System.Diagnostics.Debug.WriteLine($"{e.ToString()}");
         }
     }
-    #pragma warning restore CS1998
 
-    [RelayCommand]
-    async Task InitiateBle()
+    partial void OnAudioSourceChanged(string value)
     {
-        await _bleController.InitiateAsync();
+        if (value != "")
+        {
+            AudioControlVisible = true;
+        }
+        else
+        {
+            AudioControlVisible = false;
+        }
     }
 
-    partial void OnCurrentBleStatusChanged(BleStatus value)
+    [RelayCommand]
+    async Task Test()
     {
-        if (value == BleStatus.Scanning)
-        {
-            BleStatusText = "Scanning";
-        }
-        else if (value == BleStatus.Connecting)
-        {
-            BleStatusText = "Connecting";
-        }
-        else if (value == BleStatus.Connected)
-        {
-            BleStatusText = "Connected";
-            RecordView = true;
-        }
-        else if (value == BleStatus.Disconnected)
-        {
-            BleStatusText = "Disconnected";
-            RecordView = false;
-            if (receivedFile.Length != 0)
+        using StringContent jsonContent = new(
+            JsonSerializer.Serialize(new
             {
-                // NOTE - Change in the future (Testing purpose)
-                WriteFile("/Users/2star/Downloads/sample.wav");
-            }
-        }
-    }
+                completed = true
+            }),
+            Encoding.UTF8,
+            "application/json");
 
-    private void WriteFile(string path)
-    {
-        File.WriteAllBytes(path, receivedFile);
-    }
-
-    [RelayCommand]
-    void Record()
-    {
-        if (CurrentRecordStatus == RecordStatus.Paused)
-        {
-            // Resume
-        }
-        CurrentRecordStatus = RecordStatus.Recording;
+        System.Diagnostics.Debug.WriteLine("Test1 Button clicked");
+        //await httpClientManager.GetAsync("/");
     }
 
     [RelayCommand]
-    void Stop()
+    async Task GetAudio()
     {
-        CurrentRecordStatus = RecordStatus.NotRecording;
-    }
-
-    [RelayCommand]
-    void Pause()
-    {
-        CurrentRecordStatus = RecordStatus.Paused;
-    }
-
-    partial void OnCurrentRecordStatusChanged(RecordStatus value)
-    {
-        if (value == RecordStatus.Off)
-        {
-            RecordView = false;
-        }
-        else if (value == RecordStatus.Recording)
-        {
-            RecordButtonView = false;
-            PauseButtonView = true;
-            ResumeButtonView = false;
-        }
-        else if (value == RecordStatus.NotRecording)
-        {
-            RecordButtonView = true;
-            PauseButtonView = false;
-            ResumeButtonView = false;
-        }
-        else if (value == RecordStatus.Paused)
-        {
-            RecordButtonView = false;
-            PauseButtonView = false;
-            ResumeButtonView = true;
-        }
-    }
-
-    private void StoreData(byte[] data)
-    {
-        receivedFile = DataConvertAPI.Combine(receivedFile, data);
-    }
-
-    public void Receive(BleDataMessage message)
-    {
-        MainThread.BeginInvokeOnMainThread(() =>
-        {
-            StoreData(message.Value);
-            foreach (var value in message.Value)
-            {
-                System.Diagnostics.Debug.Write($"{value} ");
-            }
-            System.Diagnostics.Debug.Write("\n");
-            System.Diagnostics.Debug.Write($"Received size: {receivedFile.Length}\n");
-        });
-    }
-
-    public void Receive(BleStatusMessage message)
-    {
-        MainThread.BeginInvokeOnMainThread(() =>
-        {
-            CurrentBleStatus = message.Value;
-        });
+        System.Diagnostics.Debug.WriteLine("GetAudio Button clicked");
+        AudioSource = await wifiController.GetAudio();
     }
 }
