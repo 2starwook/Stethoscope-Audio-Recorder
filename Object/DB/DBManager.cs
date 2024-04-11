5e069059cb5c0f0d1c0688144eb1cc8e027f0405
Isaac Lee <2starwook@gmail.com>
2024-04-11 14:40:47 -0500

    Add checking loaded boolean mechanism

--- Object/DB/DBManager.cs
+++ Object/DB/DBManager.cs
@@ -9,32 +9,50 @@ public class DBManager
     // Manage Database: Add/Remove/Modify data
 	public DBManager()
     {
-        this._patientsManager = new PatientsManager<Patient>();
-        this._recordsManager = new RecordsManager<Record>();
+        this.patientsManager = new PatientsManager<Patient>();
+        this.recordsManager = new RecordsManager<Record>();
         this.currentRecords = new Dictionary<string, Record>();
-	}
+        this.isRecordDataLoaded = false;
+        this.isPatientDataLoaded = false;
+    }
 
-    private PatientsManager<Patient> _patientsManager;
-    private RecordsManager<Record> _recordsManager;
+    private PatientsManager<Patient> patientsManager;
+    private RecordsManager<Record> recordsManager;
+    private bool isRecordDataLoaded;
+    private bool isPatientDataLoaded;
 
     public Dictionary<string, Record> currentRecords;
     public Dictionary<string, Patient> currentPatients;
 
+    public async Task LoadDataAsync()
+    {
+        await LoadRecordDataAsync();
+        await LoadPatientDataAsync();
+    }
+
     public async Task LoadRecordDataAsync()
     {
-        this.currentRecords = new Dictionary<string, Record>();
-        foreach (Record r in await _recordsManager.GetRecordsAsync())
+        if (!isRecordDataLoaded)
         {
-            this.currentRecords.Add(r.GetId(), r);
+            isRecordDataLoaded = true;
+            this.currentRecords = new Dictionary<string, Record>();
+            foreach (Record r in await recordsManager.GetRecordsAsync())
+            {
+                this.currentRecords.Add(r.GetId(), r);
+            }
         }
     }
 
     public async Task LoadPatientDataAsync()
     {
-        this.currentPatients = new Dictionary<string, Patient>();
-        foreach (Patient p in await _patientsManager.GetPatientsAsync())
+        if (!isPatientDataLoaded)
         {
-            this.currentPatients.Add(p.GetId(), p);
+            isPatientDataLoaded = true;
+            this.currentPatients = new Dictionary<string, Patient>();
+            foreach (Patient p in await patientsManager.GetPatientsAsync())
+            {
+                this.currentPatients.Add(p.GetId(), p);
+            }
         }
     }
 
@@ -42,28 +60,28 @@ public class DBManager
                                              string recordName)
     {
         var record = new Record(recordName, File.ReadAllBytes(audioFilePath), null);
-        await _recordsManager.InsertRecordAsync(record);
+        await recordsManager.InsertRecordAsync(record);
         currentRecords.Add(record.GetId(), record);
         return record.GetId();
     }
 
     public async Task DeleteRecordAsync(string id)
     {
-        await _recordsManager.DeleteRecordAsync(id);
+        await recordsManager.DeleteRecordAsync(id);
         currentRecords.Remove(id);
     }
 
     public async Task<string> AddPatientAsync(string firstName, string lastName)
     {
         var patient = new Patient(firstName, lastName);
-        await _patientsManager.InsertPatientAsync(patient);
+        await patientsManager.InsertPatientAsync(patient);
         currentPatients.Add(patient.GetId(), patient);
         return patient.GetId();
     }
 
     public async Task DeletePatientAsync(string id)
     {
-        await _patientsManager.DeletePatientAsync(id);
+        await patientsManager.DeletePatientAsync(id);
         currentPatients.Remove(id);
     }
 
@@ -75,14 +93,5 @@ public class DBManager
     public void DetachAssignedPatientFromRecord(string audioFilePath)
     {
         // TODO - Implement Later
-    }
-
-    public async Task<string> ImportAudioFile()
-    {
-        var srcPath = await StorageAPI.GetFilePath();
-        var filename = FileAPI.GetUniqueID() + ".wav";
-        var dstPath = Path.Combine(Config.dataDirPath, filename);
-        File.Copy(srcPath.ToString(), dstPath);
-        return dstPath;
     }
 }
