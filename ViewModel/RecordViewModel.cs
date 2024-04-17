1cb0deabf4648e51397a449bbfe5a79685aacb44
Isaac Lee <2starwook@gmail.com>
2024-04-16 19:49:55 -0500

    Use AudioControl

--- ViewModel/RecordViewModel.cs
+++ ViewModel/RecordViewModel.cs
@@ -1,8 +1,6 @@
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
-using Plugin.Maui.Audio;
 
-using NET_MAUI_BLE.Object.Audio;
 using NET_MAUI_BLE.API;
 using NET_MAUI_BLE.Object.DB;
 
@@ -12,24 +10,16 @@ namespace NET_MAUI_BLE.ViewModel;
 [QueryProperty("RecordId", "RecordId")]
 public partial class RecordViewModel : ObservableObject
 {
-    public RecordViewModel(IAudioManager audioManager)
+    public RecordViewModel()
     {
-        _audioController = new AudioController(audioManager);
         _databaseManager = DependencyService.Get<DBManager>();
-        PlayingView = false;
-        PauseResumeText = "Pause";
     }
-    // TODO - Replace it with AudioControl
-    // TODO - Add Changing record name
 
-    private AudioController _audioController;
     private DBManager _databaseManager;
     [ObservableProperty]
 	private string recordId;
     [ObservableProperty]
-    private bool playingView;
-    [ObservableProperty]
-    private string pauseResumeText;
+    private string audioSource;
 
     [RelayCommand]
     void Appearing()
@@ -37,6 +27,10 @@ public partial class RecordViewModel : ObservableObject
         try
         {
             UIAPI.HideTab();
+            var fileName = $"{RecordId}.wav";
+            var binaryData = _databaseManager.currentRecords[RecordId].BinaryData;
+            FileAPI.WriteCacheData(fileName, binaryData);
+            AudioSource = FileAPI.GetCachePath(fileName);
         }
         catch (Exception e)
         {
@@ -55,52 +49,5 @@ public partial class RecordViewModel : ObservableObject
         {
             System.Diagnostics.Debug.WriteLine($"{e}");
         }
-    }
-
-    [RelayCommand]
-    async Task Share(string recordId)
-    {
-        var tempName = "file.wav";
-        var binaryData = _databaseManager.currentRecords[recordId].BinaryData;
-        await StorageAPI.ExportData(tempName, binaryData);
-    }
-
-    [RelayCommand]
-	async Task Play(string recordId)
-    {
-        PlayingView = true;
-        var fileName = $"{recordId}.wav";
-        var binaryData = _databaseManager.currentRecords[recordId].BinaryData;
-        FileAPI.WriteCacheData(fileName, binaryData);
-        await _audioController.OpenFile(FileAPI.GetCachePath(fileName));
-        _audioController.Play();
-        _audioController.AddEventHandler(new EventHandler(HandlePlayEnded));
-    }
-
-    void HandlePlayEnded(object sender, EventArgs e)
-    {
-        PlayingView = false;
-    }
-
-    [RelayCommand]
-    void PauseResume()
-    {
-        if (_audioController.IsPlaying())
-        {
-            _audioController.Pause();
-            PauseResumeText = "Resume";
-        }
-        else
-        {
-            _audioController.Play();
-            PauseResumeText = "Pause";
-        }
-    }
-
-    [RelayCommand]
-    void Stop()
-    {
-        PlayingView = false;
-        _audioController.Stop();
     }
 }
