fb20c5d32bdda55b641dadfcfe5cffef2610eaf1
Isaac Lee <2starwook@gmail.com>
2024-04-23 15:09:31 -0500

    Update HomePage with showing result text

--- Pages/HomePage.xaml.cs
+++ Pages/HomePage.xaml.cs
@@ -8,7 +8,24 @@ public partial class HomePage : ContentPage
 
 	public HomePage(HomeViewModel vm)
 	{
+		vm.AudioReceivedEvent += async (isSuccessful) => { await FadeInOut(isSuccessful); };
 		InitializeComponent();
 		BindingContext = vm;
     }
+
+    public async Task FadeInOut(bool isSuccessful)
+	{
+		if (isSuccessful == true)
+		{
+			ResultMessage.Text = "Received";
+        }
+		else
+		{
+			ResultMessage.Text = "Failed to receive";
+        }
+        await ResultMessage.FadeTo(1f, 1000);
+        await Task.Delay(2000);
+        await ResultMessage.FadeTo(0f, 1000);
+    }
+
 }
--- ViewModel/HomeViewModel.cs
+++ ViewModel/HomeViewModel.cs
@@ -1,8 +1,6 @@
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using CommunityToolkit.Mvvm.Messaging;
-using System.Text;
-using System.Text.Json;
 
 using NET_MAUI_BLE.Object.Wifi;
 using NET_MAUI_BLE.Object.DB;
@@ -35,34 +33,24 @@ public partial class HomeViewModel : ObservableObject
         }
     }
 
-    partial void OnAudioSourceChanged(string value)
-    {
-        if (value != "")
-        {
-            AudioControlVisible = true;
-        }
-        else
-        {
-            AudioControlVisible = false;
-        }
-    }
-
-    [RelayCommand]
-    async Task Test()
-    {
-        using StringContent jsonContent = new(
-            JsonSerializer.Serialize(new
-            {
-                completed = true
-            }),
-            Encoding.UTF8,
-            "application/json");
-
-        System.Diagnostics.Debug.WriteLine("Test Button clicked");
-        //await httpClientManager.GetAsync("/");
-    }
-
     [RelayCommand]
     async Task GetAudio()
     {
         Trace.WriteLine("GetAudio Button clicked");
+        bool isSuccessful = true;
+        try
+        {
+            AudioSource = await wifiController.GetAudio();
+            var recordName = $"recording_{TimeAPI.GetCurrentDateTime()}";
+            var recordId = await databaseManager.AddRecordAsync(AudioSource, recordName);
+            WeakReferenceMessenger.Default.Send(new AddRecordMessage(recordId));
+        }
+        catch (Exception e)
+        {
+            Debug.WriteLine(e);
+            isSuccessful = false;
+        }
+        AudioReceivedEvent.Invoke(isSuccessful);
+    }
+
+}
