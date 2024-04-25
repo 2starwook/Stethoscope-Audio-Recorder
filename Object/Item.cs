d3eaed636cfcda740243529c8f990f079283b218
Isaac Lee <2starwook@gmail.com>
2024-04-25 13:34:48 -0500

    Add Realm Item

---
+++ Object/Item.cs
@@ -0,0 +1,43 @@
+﻿using MongoDB.Bson;
+using Realms;
+using NET_MAUI_BLE.Services;
+
+namespace NET_MAUI_BLE.Models;
+
+public partial class Item_ : IRealmObject
+{
+    [PrimaryKey]
+    [MapTo("_id")]
+    public ObjectId Id { get; set; } = ObjectId.GenerateNewId();
+
+    [MapTo("owner_id")]
+    [Required]
+    public string OwnerId { get; set; }
+
+    [MapTo("RecordName")]
+    [Required]
+    public string RecordName { get; set; }
+
+    [MapTo("BinaryData")]
+    [Required]
+    public byte[] BinaryData { get; set; }
+
+    [MapTo("summary")]
+    [Required]
+    public string Summary { get; set; }
+
+    [MapTo("isComplete")]
+    public bool IsComplete { get; set; }
+
+    public bool IsMine => OwnerId == RealmService.CurrentUser.Id;
+
+    public string GetId()
+    {
+        return Id.ToString();
+    }
+
+    public void SetId(string id)
+    {
+        Id = new ObjectId(id);
+    }
+}
