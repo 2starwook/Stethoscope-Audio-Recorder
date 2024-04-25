a0beac0810cff570aebd077353b4b5cb086c3d8d
Isaac Lee <2starwook@gmail.com>
2024-04-25 15:08:56 -0500

    Add RealmAPI

---
+++ API/RealmAPI.cs
@@ -0,0 +1,40 @@
+﻿using Realms;
+
+using NET_MAUI_BLE.Models;
+using NET_MAUI_BLE.Services;
+
+
+namespace NET_MAUI_BLE.API;
+
+public static class RealmAPI
+{
+    public static async Task Add(Realm realm, string recordName, byte[] binaryData)
+    {
+        await realm.WriteAsync(() =>
+        {
+            var new_item = new Item_()
+            {
+                OwnerId = RealmService.CurrentUser.Id,
+                RecordName = recordName,
+                BinaryData = binaryData
+            };
+
+            realm.Add(new_item);
+        });
+    }
+
+    public static async Task Delete(Realm realm, Item_ item)
+    {
+        await realm.WriteAsync(() =>
+        {
+            realm.Remove(item);
+        });
+    }
+
+    public static IQueryable<Item_> GetAll(Realm realm)
+    {
+        return realm.All<Item_>().OrderBy(i => i.Id);
+    }
+
+}
+
